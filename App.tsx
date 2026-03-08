16ae6af3b3585363328b05fa7e696a0468ef9bcc
chokolattee <marypauline303@gmail.com>
2026-03-08 22:58:35 +0800

    fix icons

--- App.tsx
+++ App.tsx
@@ -4,13 +4,16 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './Navigation/AppNavigator';
 import axios from 'axios';
 import { useFonts } from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 export default function App() {
-  const [fontsLoaded] = useFonts(Ionicons.font);
+  const [fontsLoaded] = useFonts({
+    ...Ionicons.font,
+    ...MaterialCommunityIcons.font,
+  });
 
   if (!fontsLoaded) return null;
 
