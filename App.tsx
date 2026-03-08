776fc4d86c1dd6818cd80f5b6ee4e633e64e4edb
chokolattee <marypauline303@gmail.com>
2026-03-08 22:53:48 +0800

    fix app

--- App.tsx
+++ App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './Navigation/AppNavigator';
-import axios from 'axios';  
+import axios from 'axios';
+import { useFonts } from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 export default function App() {
+  const [fontsLoaded] = useFonts(Ionicons.font);
+
+  if (!fontsLoaded) return null;
+
   return (
     <SafeAreaProvider>
       <StatusBar style="auto" />
