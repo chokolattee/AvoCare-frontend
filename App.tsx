8179cb8950e76310cfab1f606e04bf52feef8482
chokolattee <marypauline303@gmail.com>
2026-03-08 23:10:30 +0800

    fix app

--- App.tsx
+++ App.tsx
@@ -4,17 +4,20 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './Navigation/AppNavigator';
 import axios from 'axios';
 import { useFonts } from 'expo-font';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Ionicons: require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/Ionicons.ttf'),
-    MaterialCommunityIcons: require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf'),
+  const [fontsLoaded, fontError] = useFonts({
+    ...Ionicons.font,
+    ...MaterialCommunityIcons.font,
   });
 
-  if (!fontsLoaded) return null;
+  // Wait for fonts, but don't block forever if there's an error
+  if (!fontsLoaded && !fontError) return null;
 
   return (
     <SafeAreaProvider>
